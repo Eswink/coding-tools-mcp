962911780875d03cb964c9bd8cf973be3a29e14a
Eswlnk <1525566427@qq.com>
2026-09-15 01:28:06 +0800

    feat(linux): register startup recovery commands

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -19,7 +19,7 @@ pub(crate) mod ui_memory;
 pub(crate) mod window_chrome;
 mod workspace;
 
-pub use app_info::{check_app_update, open_url};
+pub use app_info::{check_app_update, get_startup_status, open_url, retry_startup};
 pub use ui_memory::{get_webview_memory_sample, recreate_ui_webview};
 pub use window_chrome::{hide_to_tray, quit_app, show_main_window};
 pub use frp_profiles::{
